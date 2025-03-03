4819d8472de72b3bad373be6a2bd9a98e65ae24a
telavip <megateca@outlook.com>
2025-03-03 06:37:46 -0300

    Add files via upload

---
+++ links.js
@@ -0,0 +1,8 @@
+    const specialLinks = {
+        "mx": { "54545": "https://www.youtube.com/watch?v=dQw4w9WgXcQ" },
+        "fm": { "filemon1": "https://www.youtube.com/watch?v=dQw4w9WgXcQ" },
+        "st": { "streamtape1": "https://www.youtube.com/watch?v=9bZkp7q19f0" },
+        "mx_leg": { "leg54545": "https://www.youtube.com/watch?v=kJQP7kiw5Fk" },
+        "fm_leg": { "legfilemon1": "https://www.youtube.com/watch?v=9bZkp7q19f0" },
+        "st_leg": { "legstreamtape1": "https://www.youtube.com/watch?v=k2naqVqvnIk" }
+    };
---
+++ linksmv.js
@@ -0,0 +1,8 @@
+    const specialLinks = {
+        "mmx": { "54545": "https://www.youtube.com/watch?v=dQw4w9WgXcQ" },
+        "mfm": { "filemon1": "https://www.youtube.com/watch?v=dQw4w9WgXcQ" },
+        "mst": { "streamtape1": "https://www.youtube.com/watch?v=9bZkp7q19f0" },
+        "mmx_leg": { "leg54545": "https://www.youtube.com/watch?v=kJQP7kiw5Fk" },
+        "mfm_leg": { "legfilemon1": "https://www.youtube.com/watch?v=9bZkp7q19f0" },
+        "mst_leg": { "legstreamtape1": "https://www.youtube.com/watch?v=k2naqVqvnIk" }
+    };
